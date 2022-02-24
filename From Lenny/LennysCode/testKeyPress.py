35af2bea3838e498653c32a3e02a2e6e7ea358fe
SkelXton <57548041+SkelXton@users.noreply.github.com>
2022-02-23 18:46:25 -0700

    Update testKeyPress.py

--- From Lenny/LennysCode/testKeyPress.py
+++ From Lenny/LennysCode/testKeyPress.py
@@ -1,11 +1,20 @@
 # https://sshkeyboard.readthedocs.io/en/latest/
 
 from sshkeyboard import listen_keyboard
+import motor
 
 print("\nPress P to quit")
 
 def press(key):
     print(f"'{key}' pressed")
+    if key == 'w':
+        motor.directionTest(0)
+    if key == 's':
+        motor.directionTest(1)
+    if key == 'a':
+        motor.directionTest(2)
+    if key == 'd':
+        motor.directionTest(3)
         
 def release(key):
     print(f"'{key}' released")
